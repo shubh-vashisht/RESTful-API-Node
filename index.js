d208136f7b0c4e2650e1f675715e995009c1f043
Shubh <shubhv2@gmail.com>
2021-05-15 18:43:21 +0530

    Added further details for index.js for each method in order to help understand the methods better. This is 2nd commit for this file

--- index.js
+++ index.js
@@ -6,19 +6,24 @@ app.use(express.json());
 const employees = [
     { id: 1, name: "Liam ", age: "34" },
     { id: 2, name: "Richard ", age: "48" },
-    { id: 3, name: "Sam ", age: "69" }
+    { id: 3, name: "Sam ", age: "69" },
+    { id: 4, name: "Felix ", age: "29" },
+    { id: 5, name: "Jonathan ", age: "21" },
+    { id: 6, name: "Richard ", age: "38" }
 ];
-let ids = 3;
+let ids = employees.length;
 
+// the get command for root url
 app.get('/', function (req, res) {
     res.send("Hello This is ShubhVashisht's Restful Api for Employee Database");
 });
 
+// the get command in order to fullfill http get requests for all employees
 app.get('/api/employees', function (req, res) {
     res.send(employees);
 });
 
-
+//the post command for http post requests, to create a new employee
 app.post('/api/employees', function (req, res) {
     const { error } = empvalidate(req.body); //
     if (error) return res.status(400).send(error.details[0].message);
@@ -32,12 +37,14 @@ app.post('/api/employees', function (req, res) {
     res.send(emp);
 });
 
+//get command for specific employee
 app.get('/api/employees/:id', function (req, res) {
     let emp = employees.find(e => { return e.id === parseInt(req.params.id) });
     if (!emp) res.status('404').send('The employee with the given id does not exist');
     res.send(emp);
 });
 
+// http put request for updating a certain employees data
 app.put('/api/employees/:id', function (req, res) {
     const emp = employees.find(e => e.id === parseInt(req.params.id));
     if (!emp) return res.status(404).send("No such employee existssss");
@@ -59,12 +66,14 @@ function empvalidate(e) {
     return Joi.validate(e, schema);
 }
 
+// http delete command in order to delete an employees data
 app.delete('/api/employees/:id', function (req, res) {
     const emp = employees.find(e => e.id === parseInt(req.params.id));
     if (!emp) return res.status(404).send("No such employee existssss");
     res.send(deleter(employees, parseInt(req.params.id)));
 });
 
+// helper function used
 function deleter(arr, ele) {
     let temp = 0;
     for (let i = 0; i < arr.length; ++i) {
